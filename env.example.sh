13ab7769cfd5ac3f38fe7863c2a80a2392621923
Ernest Okot <okot08@gmail.com>
2017-10-11 04:36:58 +0300

    Trying out env file from compose

--- env.example.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-export DOMAIN=local
-export PORTAINER_DOMAIN=portainer.${DOMAIN}
-export TRAEFIK_UI_DOMAIN=proxy.${DOMAIN}
-export CONSUL_UI_DOMAIN=consul.${DOMAIN}
-export GITLAB_DOMAIN=gitlab.${DOMAIN}
