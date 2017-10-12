4105281ef58d3e991719d2f792cac33aeb1d0772
Ernest Okot <okot08@gmail.com>
2017-10-13 01:46:37 +0300

    Added enhancements to deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,31 +1,39 @@
 #!/bin/sh
 
-echo "\n\n1. Applying configuration from env.sh"
+echo -e  "\n\n1. Applying configuration from env.sh\n"
 
 if [ -f env.sh ]; then
     ./env.sh
+else
+    echo "--> No configuration found"
 fi
 
-echo "\n\n2. Creating global networks"
+echo -e  "\n\n2. Creating global networks\n"
 
 NETWORKS=$(docker network ls)
 
-if [ grep -q 'proxy' <<< $NETWORKS ]; then
+echo -e  "Creating proxy network"
+if [ grep "proxy\s*overlay" <<< "$NETWORKS" ]; then
+    echo -e  "--> proxy network already exists"
+else
     docker network create -d overlay proxy
 fi
 
-if [ grep -q 'database' <<< $NETWORKS ]; then
+echo -e  "Creating database network"
+if [ grep "database\s*overlay" <<< "$NETWORKS" ]; then
+    echo -e  "--> database network already exists"
+else
     docker network create -d overlay database
 fi
 
-echo "\n\n3. Stopping removed stacks"
+echo -e  "\n\n3. Stopping removed stacks\n"
 
 if [ -f deployments.txt ]; then
 
     while read p; do
 
         if [ ! -d $p ]; then
-            echo "--> Removing ${p}"
+            echo -e  "--> Removing ${p}"
             docker stack rm "${p}"
         fi
 
@@ -35,15 +43,15 @@ if [ -f deployments.txt ]; then
 
 fi
 
-echo "\n\n4. Deploying current stacks"
+echo -e  "\n\n4. Deploying current stacks\n"
 
 for D in *; do
 
     if [ -d "${D}" ]; then
-        echo "--> Deploying ${D}"
-        docker stack deploy -c "./${D}/docker-compose.yml" "${D}" --with-registry-auth
-        echo "\n"
-        echo "${D}" >> deployments.txt
+        echo -e  "--> Deploying ${D}"
+        # docker stack deploy -c "./${D}/docker-compose.yml" "${D}" --with-registry-auth
+        echo -e  "\n"
+        echo -e  "${D}" >> deployments.txt
     fi
 
 done
